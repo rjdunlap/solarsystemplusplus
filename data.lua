8bd6e0369bf9d8fe9004f3fb9a494cc5496816a1
rjdunlap <rjdunlap@outlook.com>
2025-01-25 14:41:26 -0800

    0.0.1 WIP

--- data.lua
+++ data.lua
@@ -1,20 +1,26 @@
-local nauvisOptions = { 'nauvis', 'mickora'}
+local vanillaPlanets = {'nauvis', 'vulcanus', 'gleba', 'fulgora', 'aquilo'}
+local nauvisOptions = { 'nauvis', 'mickora', 'hexalith', 'quadromire'}
 local vulcanusOptions = { 'vulcanus', 'froodara'}
-local glebaOptions = { 'gleba', 'nekohaven', 'quadromire'}
-local fulgoraOptions = { 'fulgora', 'nekohaven'}
-local aqulioOptions = { 'aqulio'}
+local glebaOptions = { 'gleba', 'nekohaven', 'tapatrion'}
+local fulgoraOptions = { 'fulgora', 'nekohaven', 'tchekor'}
+local aqulioOptions = { 'aquilo', 'tapatrion'}
 local extraContentOptions =  {}
-local t3Options = {}
+local t3Options = {'hexalith'}
 local tZeroOptions = { 'akularis', 'gerkizia'}
 local selectedPlanets = {}
 
 local function getRandomPlanet(options)
+    -- needs to use map seed
     return options[math.random(1, #options)]
 end
 
 local function hideOtherPlanets(options, selected)
     for _, option in pairs(options) do
-        if option ~= selected then
+        if option ~= selected and option ~= nil then
+            if option == 'nauvis' then
+                game.forces.player.set_surface_hidden(game.surfaces.nauvis, true)
+            end
+
             data.raw.planet[option].hidden = true
             data.raw.planet[option].map_gen_settings = nil
         end
@@ -57,38 +63,44 @@ local function moveSpaceConnections(basePlanet, selected)
 end
 
 function BuildSolarSystem()
+    -- Use from settings
+    math.randomseed(1)
     local selectedNauvis = getRandomPlanet(nauvisOptions)
     local selectedVulcanus = getRandomPlanet(vulcanusOptions)
     local selectedGleba = getRandomPlanet(glebaOptions)
     local selectedFulgora = getRandomPlanet(fulgoraOptions)
     local selectedAqulio = getRandomPlanet(aqulioOptions)
     local selectedTZero = getRandomPlanet(tZeroOptions)
+    --local selectedExtraContent = getRandomPlanet(extraContentOptions)
 
     table.insert(selectedPlanets, selectedNauvis)
-    -- Use a T0 if Nauvis is T3
+
+    -- Use a T0 if Nauvis is a T3
     if checkIfSelectedInsideOptions(selectedNauvis, t3Options) then
         table.insert(selectedPlanets, selectedTZero)
+        --build connection from T0 to 'Nauvis'
     else
-        selectedTZero = nil
+        selectedTZero = ""
     end 
     
+    moveSpaceConnections("nauvis", selectedNauvis)
     
     if checkIfAnyContains(selectedPlanets, vulcanusOptions) then
-        selectedVulcanus = nil
+        selectedVulcanus = ""
     else
         table.insert(selectedPlanets, selectedVulcanus)
         moveSpaceConnections("vulcanus", selectedVulcanus)
     end 
 
     if checkIfAnyContains(selectedPlanets, glebaOptions) then
-        selectedGleba = nil
+        selectedGleba = ""
     else
         table.insert(selectedPlanets, selectedGleba)
         moveSpaceConnections("gleba", selectedGleba)
     end 
 
     if checkIfAnyContains(selectedPlanets, fulgoraOptions) then
-        selectedFulgora = nil
+        selectedFulgora = ""
     else
         table.insert(selectedPlanets, selectedFulgora)
         moveSpaceConnections("fulgora", selectedFulgora)
@@ -96,16 +108,20 @@ function BuildSolarSystem()
     end 
 
     if checkIfAnyContains(selectedPlanets, aqulioOptions) then
-        selectedAqulio = nil
+        selectedAqulio = ""
     else
         table.insert(selectedPlanets, selectedAqulio)
         moveSpaceConnections("aqulio", selectedAqulio)
 
     end 
 
+    hideOtherPlanets(tZeroOptions, selectedNauvis)
+    hideOtherPlanets(nauvisOptions, selectedTZero)
     hideOtherPlanets(vulcanusOptions, selectedVulcanus)
     hideOtherPlanets(glebaOptions, selectedGleba)
     hideOtherPlanets(fulgoraOptions, selectedFulgora)
-    hideOtherPlanets(nauvisOptions, selectedNauvis)
-
+    hideOtherPlanets(aqulioOptions, selectedAqulio)
+    --hideOtherPlanets(extraContentOptions, selectedExtraContent)
 end
+
+BuildSolarSystem()
