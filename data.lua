68b53d43baa65806b732bad93c3025eafbb49962
rjdunlap <rjdunlap@outlook.com>
2025-01-25 21:43:23 -0800

    add seeds and fix planet removal

--- data.lua
+++ data.lua
@@ -1,6 +1,14 @@
 local vanillaPlanets = {'nauvis', 'vulcanus', 'gleba', 'fulgora', 'aquilo'}
+local allPlanets = {'nauvis', 'vulcanus', 'gleba', 'fulgora', 'aquilo',
+                    'mickora', 'hexalith', 'quadromire',
+                    'froodara', 'tchekor',
+                    'nekohaven', 'tapatrion',
+                    'hexalith',
+                    'akularis', 'gerkizia',
+                 }
+
 local nauvisOptions = { 'nauvis', 'mickora', 'hexalith', 'quadromire'}
-local vulcanusOptions = { 'vulcanus', 'froodara'}
+local vulcanusOptions = { 'vulcanus', 'froodara', 'tchekor'}
 local glebaOptions = { 'gleba', 'nekohaven', 'tapatrion'}
 local fulgoraOptions = { 'fulgora', 'nekohaven', 'tchekor'}
 local aqulioOptions = { 'aquilo', 'tapatrion'}
@@ -10,20 +18,22 @@ local tZeroOptions = { 'akularis', 'gerkizia'}
 local selectedPlanets = {}
 
 local function getRandomPlanet(options)
-    -- needs to use map seed
+
     return options[math.random(1, #options)]
 end
 
 local function hideOtherPlanets(options, selected)
-    for _, option in pairs(options) do
-        if option ~= selected and option ~= nil then
-            if option == 'nauvis' then
-                game.forces.player.set_surface_hidden(game.surfaces.nauvis, true)
+    for _, s in pairs(selected) do
+        for i, option in pairs(options) do
+            if option == s then
+                table.remove(options, i)
             end
-
+        end
+    end
+    
+    for _, option in pairs(options) do
             data.raw.planet[option].hidden = true
             data.raw.planet[option].map_gen_settings = nil
-        end
     end
 end
 
@@ -40,15 +50,19 @@ local function checkIfAnyContains(set, set2)
     for _, selected in pairs(set) do
         for _, option in pairs(set2) do
             if option == selected then
-                return true
+                return selected
             end
         end
     end
-    return false
+    return ""
 end
 
 
-local function moveSpaceConnections(basePlanet, selected) 
+local function moveSpaceConnections(basePlanet, selected)
+    if basePlanet == selected then
+        return
+    end
+
     for _,connection in pairs (data.raw["space-connection"]) do 
         if connection == nil then
             break
@@ -63,8 +77,10 @@ local function moveSpaceConnections(basePlanet, selected)
 end
 
 function BuildSolarSystem()
-    -- Use from settings
-    math.randomseed(1)
+    -- feed seed
+
+    math.randomseed(settings.startup["solar-system-random-seed"].value)
+
     local selectedNauvis = getRandomPlanet(nauvisOptions)
     local selectedVulcanus = getRandomPlanet(vulcanusOptions)
     local selectedGleba = getRandomPlanet(glebaOptions)
@@ -74,54 +90,57 @@ function BuildSolarSystem()
     --local selectedExtraContent = getRandomPlanet(extraContentOptions)
 
     table.insert(selectedPlanets, selectedNauvis)
+    moveSpaceConnections("nauvis", selectedNauvis)
 
     -- Use a T0 if Nauvis is a T3
     if checkIfSelectedInsideOptions(selectedNauvis, t3Options) then
         table.insert(selectedPlanets, selectedTZero)
-        --build connection from T0 to 'Nauvis'
+        
+        --all T0 should already have a connection Nauvis?
     else
         selectedTZero = ""
-    end 
-    
+    end
+
     moveSpaceConnections("nauvis", selectedNauvis)
-    
-    if checkIfAnyContains(selectedPlanets, vulcanusOptions) then
-        selectedVulcanus = ""
+
+    local tmpVul = checkIfAnyContains(selectedPlanets, vulcanusOptions)
+    if tmpVul ~= "" then
+        selectedVulcanus = tmpVul
+        moveSpaceConnections("vulcanus", selectedVulcanus)
     else
         table.insert(selectedPlanets, selectedVulcanus)
         moveSpaceConnections("vulcanus", selectedVulcanus)
     end 
 
-    if checkIfAnyContains(selectedPlanets, glebaOptions) then
-        selectedGleba = ""
+    local tmpGleba = checkIfAnyContains(selectedPlanets, glebaOptions)
+    if tmpGleba ~= "" then
+        selectedGleba = tmpGleba
+        moveSpaceConnections("gleba", selectedGleba)
     else
         table.insert(selectedPlanets, selectedGleba)
         moveSpaceConnections("gleba", selectedGleba)
     end 
 
-    if checkIfAnyContains(selectedPlanets, fulgoraOptions) then
-        selectedFulgora = ""
+    local tmpFulgora = checkIfAnyContains(selectedPlanets, fulgoraOptions)
+    if tmpFulgora ~= "" then
+        selectedFulgora = tmpFulgora
+        moveSpaceConnections("fulgora", selectedFulgora)
     else
         table.insert(selectedPlanets, selectedFulgora)
         moveSpaceConnections("fulgora", selectedFulgora)
+    end
 
-    end 
-
-    if checkIfAnyContains(selectedPlanets, aqulioOptions) then
-        selectedAqulio = ""
+    local tmpAqulio = checkIfAnyContains(selectedPlanets, aqulioOptions)
+    if tmpAqulio ~= "" then
+        selectedAqulio = tmpAqulio
+        moveSpaceConnections("aqulio", selectedAqulio)
     else
         table.insert(selectedPlanets, selectedAqulio)
         moveSpaceConnections("aqulio", selectedAqulio)
+    end
 
-    end 
-
-    hideOtherPlanets(tZeroOptions, selectedNauvis)
-    hideOtherPlanets(nauvisOptions, selectedTZero)
-    hideOtherPlanets(vulcanusOptions, selectedVulcanus)
-    hideOtherPlanets(glebaOptions, selectedGleba)
-    hideOtherPlanets(fulgoraOptions, selectedFulgora)
-    hideOtherPlanets(aqulioOptions, selectedAqulio)
-    --hideOtherPlanets(extraContentOptions, selectedExtraContent)
+    log(serpent.block(selectedPlanets))
+    hideOtherPlanets(allPlanets, selectedPlanets)
 end
 
 BuildSolarSystem()
---
+++ settings.lua
@@ -0,0 +1,12 @@
+data:extend(
+{
+    {
+      type = "int-setting",
+      name = "solar-system-random-seed",
+      setting_type = "startup",
+      default_value = 5123,
+      minimum_value = 0,
+      maximum_value = 1000000,
+      order = "a",
+   }
+})
